a74fb34a55f656c03494fdff1744e76615801d62
Rajat Verma <rajat311201@gmail.com>
2022-06-24 23:45:07 +0530

    corrected port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
     })
     .then(() => console.log("DB connection successful!"));
 
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
